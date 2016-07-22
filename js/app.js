393f51bb9b400a278b540e16d9c46e171fcd0389
derek_robertson <dwilliamrobertson@gmail.com>
2016-07-21 22:32:36 -0700

    moves and highlights in red if wrong

--- js/app.js
+++ js/app.js
@@ -4,3 +4,30 @@
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+
+var myApp = angular.module('FandV', []);
+
+myApp.controller('appCtrl', ['$scope', function($scope){
+  $scope.fruits = [];
+  $scope.vegetables = [];
+  $scope.both = fruits.concat(vegetables);
+  $scope.click = function(destination, idx, currentArray){
+    $scope[destination].push($scope[currentArray][idx]);
+    $scope[currentArray].splice(idx,1);
+    
+  }
+
+  $scope.checkCorrect = function(currentItem, currentArray){
+    if(currentArray === 'fruits'){
+      if(fruits.indexOf(currentItem) === -1){
+        return true;
+      }
+    } else {
+      if(currentArray === 'vegetables'){
+        if(vegetables.indexOf(currentItem) === -1){
+          return true;
+        }
+      }
+    }
+  }
+}]);
